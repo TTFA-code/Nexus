05b54d9c8ca83ed40c7fdaf070856b8c48b11b82
L0kkiii <your-github-jonathanrthompson@outlook.com>
2026-01-22 19:48:42 -0400

    fixed moblie expericenc

--- web/app/manifest.ts
+++ web/app/manifest.ts
@@ -11,14 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#00f3ff',
         icons: [
             {
-                src: '/icon-192x192.png',
+                src: '/favicon.ico',
                 sizes: '192x192',
-                type: 'image/png',
+                type: 'image/x-icon',
             },
             {
-                src: '/icon-512x512.png',
+                src: '/favicon.ico',
                 sizes: '512x512',
-                type: 'image/png',
+                type: 'image/x-icon',
             },
         ],
     }
